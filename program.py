8ded839f939161683c91f2cc8cdce9c5feaeb91a
Azeem <azeema055@gmail.com>
2025-01-06 19:48:18 +0500

    DSA Array practice

---
+++ program.py
@@ -0,0 +1,11 @@
+#Data structure & Algorithms Practice
+
+# Python Example: Create an array and display its elements
+def display_arr():
+    arr1 = [1,2,3,4,5,6,7]
+    print("Array elements: ")
+    for i in arr1:
+        print(i, end=' ')
+
+#function call
+display_arr()
